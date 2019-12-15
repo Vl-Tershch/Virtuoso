7afbc4f51596a965f6804fa06d4b98c545ee4ea1
Tereshchenko Vlad <ddiavall@yandex.ru>
2019-12-16 02:06:01 +0300

    Added test for user model v2.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,16 @@ require 'minitest/autorun'
 describe User do
   it 'can not have empty login' do
     role1 = Role.create(name: 'User')
-    _(User.create(login: nil, password: '12345', email: 'ddiavall@yandex.ru', role: role1, telephone_number: '89281800979').valid?).must_equal false
+    _(User.create(login: nil, password: '12345', email: 'i@yandex.ru', role: role1, telephone_number: '892818000').valid?).must_equal false
   end
 
+  it 'can not have empty passsword' do
+    role1 = Role.create(name: 'User')
+    _(User.create(login: 'Oleg', password: nil, email: 'oleg@yandex.ru', role: role1, telephone_number: '892818000').valid?).must_equal false
+  end
+
+  it 'can not have empty email' do
+    role1 = Role.create(name: 'User')
+    _(User.create(login: 'Oleg', password: '12345', email: nil, role: role1, telephone_number: '892818000').valid?).must_equal false
+  end
 end
